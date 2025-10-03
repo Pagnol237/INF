429211fe67b8e021bbc5647badf0ec99d9ad2067
Hey there! I'm franck pagnol Nounjo <99295070+Pagnol237@users.noreply.github.com>
2025-10-03 12:26:00 -0500

    first commit
    push all filles

---
+++ Myapp/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Runtime.CompilerServices;
+
+Console.WriteLine("a partir de ce jour je reussi dans ce que j\'entreprend sigé franck pagnol");
+
+
+IEnumerable<int> nbrPair()
+{
+    
+    for (int i=1; i <= 10; i++)
+    {
+        if (i % 2 == 0)
+        {
+            yield return i;
+        }
+    }
+}
+
+foreach (int s in nbrPair())
+{
+    Console.WriteLine(s);
+}
+
